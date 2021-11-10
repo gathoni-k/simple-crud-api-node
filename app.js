004f03dc5a16e1e3dcfe0720529b69179a85c909
gathoni-k <maryg.kahiga@gmail.com>
2021-11-10 15:31:24 +0300

    Add routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const { urlencoded } = require('express')
 
 const app = express()
 const db = require('./db')
+
+// import book schema
+const Book = require('./bookSchema')
 app.use(express.json()) // pass json objects
 app.use(urlencoded({ extended: true})) // pass url encoded objects
 
@@ -14,28 +17,96 @@ app.get('/', (req, res) => {
 })
 
 // create
-app.post('/create', () => {
+app.post('/create', (req, res) => {
+    // check if book info was passed in
+    if (!Object.keys(req.body).length) {
+        res.status(400).json({
+            success: 'false',
+            message: 'Provide book data'
+        })
+    }
 
+    const {title, author, publisher, read} = (req.body)
+    const book = new Book({
+        title,
+        author,
+        publisher,
+        read
+    })
+    book.save().then((data) => {
+        res.status(201).json({
+            success: true,
+            message: 'Created'
+        })
+
+    }).catch(err => {
+        res.status().json({
+            success: false,
+            message: err.message
+        })
+    })
+    // create a book
 })
 
 // read all
-app.get('/retrieve-all', () => {
-
+app.get('/retrieve-all', (req ,res) => {
+    Book.find({}, (err, data) => {
+        if (err) {
+            res.status(404).json({
+                success: false,
+                books: null
+            })
+        }
+        res.status(200).json({
+            success: true,
+            books: data
+        })
+    })
 })
 
 // get one
 
-app.get('/retrieve/:bookID', () => {
-
+app.get('/retrieve/:bookID', (req, res) => {
+    Book.findById(req.params.bookID, (err, data) => {
+        if (err) {
+            res.status(404).json({
+                success: false,
+                book: null
+            })
+        }
+        res.status(200).json({
+            success: true,
+            book: data
+        })
+    })
 })
 
 // update, 
-app.put('/edit/:bookID', () => {
-
+app.put('/edit/:bookID', (req, res) => {
+    Book.findByIdAndUpdate(req.params.bookID,req.body, {new:true}, (err, result) => {
+        if (err) {
+            res.status(404).json({
+                success: false,
+            })
+        }
+        res.status(200).json({
+            success: true,
+            book: result
+        })
+    })
 })
 // delete
-app.delete('/delete/:bookID', () => {
-
+app.delete('/delete/:bookID', (req, res) => {
+    Book.findByIdAndDelete(req.params.bookID, (err, result) => {
+        if (err) {
+            res.status(404).json({
+                success: false,
+            })
+        }
+        res.status(200).json({
+            success: true,
+        })
+    })
 })
 
 
